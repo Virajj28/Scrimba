879ed1335ab843e4b82d2f6d592104412f41e1fc
Virajj28 <viraj28j@gmail.com>
2022-02-22 20:00:25 +0530

    fnctn invoked

--- 05Learn JS/01OnCode.js
+++ 05Learn JS/01OnCode.js
@@ -1,11 +1,20 @@
 //  Writing a first variable
 
-let myAge = 2;
-console.log(myAge)
+// let myAge = 2;
+// console.log(myAge)
 
 // function increment
-<button id="incr-btn">Increment</button>
+{/* <button id="incr-btn">Increment</button>
 
 function increment () {
     console.log("the btn is clickd")
+} */}
+
+// To less write code for console we use functions and then it is invoked.
+function countdown () {
+    console.log(2)
+    console.log(1)
 }
+
+countdown();
+countdown();
