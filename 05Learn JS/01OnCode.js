e76dc260866e9dd76ccf551cff1326c55cc44c06
Virajj28 <viraj28j@gmail.com>
2022-02-22 20:15:40 +0530

    incr display

--- 05Learn JS/01OnCode.js
+++ 05Learn JS/01OnCode.js
@@ -11,10 +11,25 @@ function increment () {
 } */}
 
 // To less write code for console we use functions and then it is invoked.
-function countdown () {
-    console.log(2)
-    console.log(1)
-}
+// function countdown () {
+//     console.log(2)
+//     console.log(1)
+// }
 
-countdown();
-countdown();
+// countdown();
+// countdown();
+
+// change the count-el in the HTML to reflect the new count
+
+// let count = 0
+
+// function increment() {
+//     document.getElementById("count-el").innerText = count + 1
+// }
+
+let countEl = document.getElementById("count-el")
+let count = 0
+function increment() {
+    count = count + 1;
+    countEl.innerText = count
+}
