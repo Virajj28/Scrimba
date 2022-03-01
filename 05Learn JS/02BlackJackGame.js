56c054dee6593856db0e11c8291f7db2848c54d1
Virajj28 <viraj28j@gmail.com>
2022-03-02 00:22:36 +0530

    DRY - Don't repeat yourself

--- 05Learn JS/02BlackJackGame.js
+++ 05Learn JS/02BlackJackGame.js
@@ -89,6 +89,13 @@ for (let i = 10; i < 101; i += 10) {
 }
 // you always forget defining variable in loop
 
+let cards = [7, 3, 9]
+// Create a for loop that logs out all the cards in the array
+// Use cards.length to specify how long the loop should run
+for (let i = 0; i < cards.length; i++) {
+    console.log(cards[i])
+}
+
 
 // // Return values in funcs
 
