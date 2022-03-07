5997952af0d616421ad2d394bcd237ed87837498
Virajj28 <viraj28j@gmail.com>
2022-03-08 00:13:57 +0530

    push val from inpt fld

--- 05Learn JS/03BuildChrmXtension/index.js
+++ 05Learn JS/03BuildChrmXtension/index.js
@@ -2,8 +2,22 @@
 // 2. Add a click event listener to the box 
 // 3. Log out "I want to open the box!" when it's clicked
 
-let box = document.getElementById("box")
+// let box = document.getElementById("box")
 
-box.addEventListener("click", function(){
-    console.log("I want to open the box!")
+// box.addEventListener("click", function(){
+//     console.log("I want to open the box!")
+// })
+
+
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    // Push the value from the inputEl into the myLeads array 
+    // instead of the hard-coded "www.awesomeleads.com" value
+    // Google -> "get value from input field javascript"
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
+
