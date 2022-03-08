809b6f0280183e5c4e6905bbc2663888829c1361
Virajj28 <viraj28j@gmail.com>
2022-03-08 17:07:02 +0530

    more innerHtml prac

--- 05Learn JS/03BuildChrmXtension/index.js
+++ 05Learn JS/03BuildChrmXtension/index.js
@@ -37,8 +37,17 @@ for (let i = 0; i < myLeads.length; i++) {
 }
 
 
-// Use .innerHTML to render a Buy! button inside the div container
-const container = document.getElementById("container")
+// // Use .innerHTML to render a Buy! button inside the div container
+// const container = document.getElementById("container")
 
-container.innerHTML = "<button>Buy!</button>"
+// container.innerHTML = "<button>Buy!</button>"
 
+
+container.innerHTML = "<button onclick='buy()'>Buy!</button>"
+
+// When clicked, render a paragraph under the button (in the container)
+// that says "Thank you for buying!"
+
+function buy(){
+    container.innerHTML += "<p>Thank you for buying!</p>"
+}
