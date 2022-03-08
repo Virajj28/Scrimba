2ce88d4b8a735a65bf5172254a397de1832a9db4
Virajj28 <viraj28j@gmail.com>
2022-03-08 18:08:51 +0530

    a tg w/ new tab link

--- 05Learn JS/03BuildChrmXtension/index.js
+++ 05Learn JS/03BuildChrmXtension/index.js
@@ -61,8 +61,12 @@ inputBtn.addEventListener("click", function() {
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
-        listItems += "<li>" + myLeads[i] + "</li>"
+        // // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
+        // listItems += "<li>" + myLeads[i] + "</li>"
+
+        // Wrap the lead in an anchor tag (<a>) inside the <li>
+        // Can you make the link open in a new tab?
+        listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
     }
     // 3. Render the listItems inside the unordered list using ulEl.innerHTML
     ulEl.innerHTML = listItems
