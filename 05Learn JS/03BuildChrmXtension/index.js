9a5cbea32ac06939ae779b2803fb997e660e3ce6
Virajj28 <viraj28j@gmail.com>
2022-03-10 17:30:18 +0530

    Json stringify & parse

--- 05Learn JS/03BuildChrmXtension/index.js
+++ 05Learn JS/03BuildChrmXtension/index.js
@@ -93,3 +93,14 @@ function renderLeads() {
 // }
 
 
+//// Storing arrays in localStorage
+
+// let myLeads = `["www.awesomelead.com"]`
+// 1. Turn the myLeads string into an array
+myLeads = JSON.parse(myLeads)
+// 2. Push a new value to the array
+myLeads.push("www.lead2.com")
+// 3. Turn the array into a string again
+myLeads = JSON.stringify(myLeads)
+// 4. Console.log the string using typeof to verify that it's a string
+console.log(typeof myLeads)
