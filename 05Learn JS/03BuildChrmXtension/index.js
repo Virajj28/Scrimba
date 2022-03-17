50048ee784ac65a8b90a13d722d9ec55354809de
Virajj28 <viraj28j@gmail.com>
2022-03-17 23:24:44 +0530

    tru||fals w/ proj

--- 05Learn JS/03BuildChrmXtension/index.js
+++ 05Learn JS/03BuildChrmXtension/index.js
@@ -127,3 +127,11 @@ localStorage.clear()
 let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
 console.log(leadsFromLocalStorage)
+
+// ["lead1", "lead2"] or null
+// 1. Check if leadsFromLocalStorage is truthy
+// 2. If so, set myLeads to its value and call renderLeads()
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
