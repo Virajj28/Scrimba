892afa3bddeef9165a849f3fece986a1865ec0dd
Virajj28 <viraj28j@gmail.com>
2022-03-23 00:12:55 +0530

    desctruct w/ rest opr to reasgn arr elems

--- 06Learn Modern JS/codes.js
+++ 06Learn Modern JS/codes.js
@@ -121,3 +121,16 @@ console.log(a);
 console.log(b); 
 
 
+//// Destructuring Assginment with rest operator to reassign array elements
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+
+  const [ , , ...arr] = list; 
+
+  return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr); 
+console.log(source);
+
+
