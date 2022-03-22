a39c561f7ff5de40a0724751acc3875e5a82090e
Virajj28 <viraj28j@gmail.com>
2022-03-23 00:04:26 +0530

    descrut to assgn var frm obj

--- 06Learn Modern JS/codes.js
+++ 06Learn Modern JS/codes.js
@@ -71,3 +71,19 @@ let arr2;
 })();
 console.log(arr2);
 
+
+//// Destructuring Assginment to assign variables from Objects
+const AVG_TEMPERATURES = {
+  today: 77.5,
+  tomorrow: 79
+};
+
+function getTempOfTmrw(avgTemperatures) {
+  "use strict";
+  // change code below this line
+  const { tomorrow : tempOfTomorrow } = avgTemperatures; // change this line
+  // change code above this line
+  return tempOfTomorrow;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURES)); // should be 79
