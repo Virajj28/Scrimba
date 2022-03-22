7c54712d8ef6d58c75a0567de686a3ad7b48f10c
Virajj28 <viraj28j@gmail.com>
2022-03-23 00:06:47 +0530

    desctruct to assgn vars frm nested objs

--- 06Learn Modern JS/codes.js
+++ 06Learn Modern JS/codes.js
@@ -87,3 +87,20 @@ function getTempOfTmrw(avgTemperatures) {
 }
 
 console.log(getTempOfTmrw(AVG_TEMPERATURES)); // should be 79
+
+
+//// Destructuring Assginment to assign variables from Nested Objects
+const LOCAL_FORECAST = {
+  today: { min: 72, max: 83 },
+  tomorrow: { min: 73.3, max: 84.6 }
+};
+
+function getMaxOfTmrw(forecast) {
+  "use strict";
+
+  const { tomorrow : { max : maxOfTomorrow }} = forecast; 
+
+  return maxOfTomorrow;
+}
+
+console.log(getMaxOfTmrw(LOCAL_FORECAST)); 
