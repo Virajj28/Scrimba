43a4938f817422e805483593d5973c3a06b5eced
Virajj28 <viraj28j@gmail.com>
2022-03-24 23:18:28 +0530

    str usng templte literal

--- 06Learn Modern JS/codes.js
+++ 06Learn Modern JS/codes.js
@@ -153,3 +153,29 @@ const half = (function() {
 })();
 console.log(stats); 
 console.log(half(stats)); 
+
+
+//// Create strings using template literals
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+  const resultDisplayArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    resultDisplayArray.push(`<li class="text-warning">${arr[i]}</li>`)
+  }
+
+  return resultDisplayArray;
+}
+/**
+ * makeList(result.failure) should return:
+ * [ `<li class="text-warning">no-var</li>`,
+ *   `<li class="text-warning">var-on-top</li>`, 
+ *   `<li class="text-warning">linebreak</li>` ]
+ **/
+const resultDisplayArray = makeList(result.failure);
+
+console.log(resultDisplayArray)
