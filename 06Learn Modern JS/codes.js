be9f70b62847e8b33cb030403820a8f00c88a441
Virajj28 <viraj28j@gmail.com>
2022-03-27 23:57:44 +0530

    gettrs n settr cntrllg accs to obj

--- 06Learn Modern JS/codes.js
+++ 06Learn Modern JS/codes.js
@@ -208,3 +208,42 @@ function makeClass() {
 const Vegetable = makeClass();
 const carrot = new Vegetable('carrot');
 console.log(carrot.name); 
+
+
+
+//// Using getters and setters to control access to an object
+class Book {
+  constructor(author) {
+    this._author = author;
+  }
+  // getter
+  get writer(){
+    return this._author;
+  }
+  // setter
+  set writer(updatedAuthor){
+    this._author = updatedAuthor;
+  }
+}
+
+function makeClass() {
+  class Thermostat {
+    constructor(temp) {
+      this._temp = 5/9 * (temp - 32);
+    }
+    get temperature(){
+      return this._temp;
+    }
+    set temperature(updatedTemp){
+      this._temp = updatedTemp;
+    }
+  }
+  return Thermostat;
+}
+
+const Thermostat = makeClass();
+const thermos = new Thermostat(76); 
+let temp = thermos.temperature; 
+thermos.temperature = 26;
+temp = thermos.temperature; 
+console.log(temp);
