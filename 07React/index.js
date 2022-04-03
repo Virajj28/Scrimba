9890beeaafc61c4781e0a649de8082eedc21e808
Virajj28 <viraj28j@gmail.com>
2022-04-03 08:07:30 +0530

    updating new list in render

--- 07React/index.js
+++ 07React/index.js
@@ -1,4 +1,8 @@
 
 ReactDOM.render(<h2>Good Mornin', Viraj!</h2>, document.getElementById("root"));
-DOM.render(<h2>Good Mornin', Viraj!</h2>, document.getElementById("root"));
+ReactDOM.render(<h2>Good Mornin', Viraj!</h2>, document.getElementById("root"));
 
+ReactDOM.render(<ul>
+    <li>Thing 1</li>
+    <li>Thing 2</li>
+    </ul>, document.getElementById("root"))
