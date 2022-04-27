a4d4d311a001ef00501c925509ee932272b1e9ed
Virajj28 <viraj28j@gmail.com>
2022-04-27 22:44:26 +0530

    memegen brushup start

---
+++ 07React/MemeGen.js/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+/**
+ * Challenge: Build the Header component
+ */
+export default function App() {
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}
