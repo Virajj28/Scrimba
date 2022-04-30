ce0b69df3edd0f3308b9cf28860ac55897554fb4
Virajj28 <viraj28j@gmail.com>
2022-04-30 23:17:53 +0530

    event listeners

--- 07React/MemeGen.js/App.js
+++ 07React/MemeGen.js/App.js
@@ -5,10 +5,25 @@ import Meme from "./components/Meme"
  * Challenge: Build the Header component
  */
 export default function App() {
+    function handleClick() {
+        console.log("clicked")
+    }
+
+    function handleOnMouseOver() {
+        console.log("MouseOver")
+    }
+
     return (
         <div>
             <Header />  
             <Meme />
+            <div>
+                <img
+                    src="https://picsum.photos/640/360" 
+                    onMouseOver={handleOnMouseOver} 
+                />
+                <button onClick={handleClick}>Click Me</button>
+            </div>
         </div>
     )
 }
